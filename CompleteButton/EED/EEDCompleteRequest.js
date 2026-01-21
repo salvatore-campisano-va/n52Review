20c67623036116f030f3752908b090f5f163b126
salvatore-campisano-va <salvatore.campisano@va.gov>
2026-01-21 12:57:24 -0500

    EED bootstrap4

--- CompleteButton/EED/EEDCompleteRequest.js
+++ CompleteButton/EED/EEDCompleteRequest.js
@@ -91,7 +91,7 @@ EEDCompleteRequest.initialize = function() {
     }
     
     this.$button.addEventListener("click", async function() {
-        if (self.$button.classList.contains("btn-loading")) return;
+        if (self.$button.disabled) return;
         
         try {
             self.setButtonLoading(true);
@@ -114,12 +114,16 @@ EEDCompleteRequest.initialize = function() {
 EEDCompleteRequest.setButtonLoading = function(isLoading) {
     if (!this.$button) return;
     
+    const spinner = this.$button.querySelector(".spinner-border");
+    
     if (isLoading) {
-        this.$button.classList.add("btn-loading");
+        this.$button.disabled = true;
         this.$button.querySelector(".button-text").textContent = "Processing...";
+        if (spinner) spinner.classList.remove("d-none");
     } else {
-        this.$button.classList.remove("btn-loading");
+        this.$button.disabled = false;
         this.$button.querySelector(".button-text").textContent = "Complete Request";
+        if (spinner) spinner.classList.add("d-none");
     }
 };
 
